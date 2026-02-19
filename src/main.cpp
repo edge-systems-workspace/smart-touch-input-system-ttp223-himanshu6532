00b3072c382ca2a51b5f818031c2f4488fd4748d
himanshu6532 <himanshu.yadav_cs.aiml24@gla.ac.in>
2026-02-19 13:33:19 +0530

    mplement digitalRead on pin 2 to detect touch state

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ int touchpin=2;
 int state=0;
 
 void setup() {
-    // pinMode(touchpin,pinMode:INPUT);
-    // Serial.begin(9600);
+    pinMode(touchpin,pinMode:INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
